e75a235dbf0abf1ea7345b20c5907fef787ef035
Chris Haining <chrishaining@yahoo.co.uk>
2019-09-10 10:45:50 +0100

    adds radio buttons that work

--- src/app.js
+++ src/app.js
@@ -5,17 +5,21 @@ document.addEventListener('DOMContentLoaded', () => {
     {el: "#app",
     data: {
       tasks: [
-        {name: "walk the dog"},
-        {name: "mow the lawn"},
+        // "walk the dog", "mow the lawn"
+        {name: "walk the dog", priority: "high"},
+        {name: "mow the lawn", priority: "low"}
       ],
-      newTask: ""
+      newTask: {name: "", priority: "low"}
     },
     methods: {
       saveNewTask: function () {
         // if (this.newTask !== "") { }
         // this.tasks.push({name: this.newTask.name, priority: this.newTask.priority});
-        this.tasks.push({name: this.newTask.name}) ;
-        this.newTask = ""
+        // this.tasks.push(name: this.newTask.name}) ;
+        // this.tasks.push({name: this.newTask.name, priority: "low"});
+        this.tasks.push(this.newTask);
+
+        this.newTask = {name: "", priority: "low"}
       },
 
     }
